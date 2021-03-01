a26a2088103c8b4eae808278002b2682d796959e
AgolaDeveloper <owinoagola00@gmail.com>
2021-03-01 23:03:50 +0300

    js manipulation

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ var employees= {
     children: 3,
     childrenNames: [
         {firstName: "Leo", lastName: "Gueth", age: 11, grade: 4, gender: "female"},
-        {firstName: "Riziki", lastName: "Hawii", age: 14, grade: 9, gender: female},
+        {firstName: "Riziki", lastName: "Hawii", age: 14, grade: 9, gender: "female"},
         {firstName: "Sibuor", lastName: "Otis", age: 7, grade: 3, gender: "male"},
-        {firstName: "Geno", lastName: "Apiyo", age: 12, grade: 5, "female"}
+        {firstName: "Geno", lastName: "Apiyo", age: 12, grade: 5, gender: "female"}
         ]    
 }
 
@@ -19,6 +19,8 @@ var emp= "<ol> <ol>";
 
 function func(){
     for(prop in employees)
+        //for(props in employees.childrenNames)
+
         emp+= "<li>"+ prop +": "+ employees[prop] +"</li><br>";
         //employees[prop];
 
