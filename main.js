662ea8889380195b608b7cc5fbc65e3ce185885c
AgolaDeveloper <owinoagola00@gmail.com>
2021-03-03 23:39:02 +0300

    objectChanges

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var employees= {
     salary: "USD 2500",
     children: 4,
     childrenNames: [
-        {firstName: "Leo", lastName: "Gueth", age: 11, grade: 4, gender: "female"},
+        {firstName: "Tene", lastName: "Gueth", age: 11, grade: 4, gender: "female"},
         {firstName: "Riziki", lastName: "Hawii", age: 14, grade: 9, gender: "female"},
         {firstName: "Sibuor", lastName: "Otis", age: 7, grade: 3, gender: "male"},
         {firstName: "Geno", lastName: "Apiyo", age: 12, grade: 5, gender: "female"}
